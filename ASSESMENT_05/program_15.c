755c1101118b2933771d6f0c4eeeb05ffa583898
SANJAY SENTHIL KUMAR <sanjay.vdm007@gmail.com>
2026-07-15 14:36:32 +0530

    Fix bugs in program 15 and 16

--- ASSESMENT_05/program_15.c
+++ ASSESMENT_05/program_15.c
@@ -1,30 +1,35 @@
 //Write a program to get three numbers from user and print the LCM of those numbers.
 
-#include<stdio.h>
+#include <stdio.h>
+
+int gcd(int a, int b);
+
 int main()
 {
-    int no1,no2,no3;
-    scanf("%d%d%d",&no1,&no2,&no3);
-    int gcd1=gcd(no1,no2);
-    int lcm1=(no1*no2)/gcd1;
-    int gcd2=gcd(lcm1,no3);
-    int lcm2=(lcm1*no3)/gcd2;
-    printf("The LCM Of The Numbers Are:%d",lcm2);
+    int no1, no2, no3;
+    
+    scanf("%d %d %d", &no1, &no2, &no3);
+    
+    int gcd1 = gcd(no1, no2);
+    int lcm1 = (no1 * no2) / gcd1;
+    
+    int gcd2 = gcd(lcm1, no3);
+    int lcm2 = (lcm1 * no3) / gcd2;
+    
+    printf("The LCM Of The Numbers Are:%d\n", lcm2);
 
-
+    return 0;
 }
 
-
-int gcd(int a,int b)
+int gcd(int a, int b)
 {
-    int i,gcd;
-    for(i=1;i<=a && i<=b;i++)
+    int i, gcd_val = 1;
+    for(i = 1; i <= a && i <= b; i++)
     {
-        if(a%i==0 && b%i==0)
+        if(a % i == 0 && b % i == 0)
         {
-            gcd=i;
+            gcd_val = i;
         }
-
     }
-    return gcd;
+    return gcd_val;
 }
